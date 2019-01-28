85075f63e88f5070c8e1a9b12b6e9ced72430e16
Emir Bojagic <emir.bojagic@serapion.net>
2019-01-28 09:09:32 +0100

    Updates

---
+++ precache-manifest.46a29aed54816b1419a19c507aa390e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b82de16836a614893cb3",
+    "url": "/static/js/main.b82de168.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b82de16836a614893cb3",
+    "url": "/static/css/main.9d04c57a.chunk.css"
+  },
+  {
+    "revision": "826dee463f7dec46e7b2d15b545562dc",
+    "url": "/index.html"
+  }
+];
